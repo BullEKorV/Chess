58c9618f09337dd1f46128e8bc5a782ef52a213e
Elias <elias.wasielewski@elev.ga.ntig.se>
2021-02-11 14:41:25 +0100

    initial commit

---
+++ Chess/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public class Piece
+{
+    public int player;
+    public int x;
+    public int y;
+    public Type pieceType;
+    public Piece(int player, int x, int y, Type pieceType)
+    {
+        this.player = player;
+        this.x = x;
+        this.y = y;
+        this.pieceType = pieceType;
+    }
+}
+enum PieceType
+{
+    King,
+    Queen,
+    Rook,
+    Bishop,
+    Knight,
+    Pawn
+}
